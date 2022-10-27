b4945e23598a8cdbc533d145d596da4d65809157
Marcos Saito <47644934+saitodeveloper@users.noreply.github.com>
2022-10-27 12:29:47 -0300

    Create recursive.js

---
+++ recursive.js
@@ -0,0 +1,44 @@
+const menus = [
+    { name: 'Menu 1', items: [
+        { name: 'Menu 1.1', items: [
+            { name: 'Menu 1.1.1', items: [] },
+            { name: 'Menu 1.1.2', items: [] }
+        ] },
+        { name: 'Menu 1.2', items: [
+            { name: 'Menu 1.2.1', items: [] },
+            { name: 'Menu 1.2.2', items: [] }
+        ] }
+    ] },
+    { name: 'Menu 2', items: [
+        { name: 'Menu 2.1', items: [
+            { name: 'Menu 2.1.1', items: [] },
+            { name: 'Menu 2.1.2', items: [] }
+        ] }
+    ]}
+]
+
+/* Primeira recursão resolve a lista */
+function solveMenus(menus, name, index = 0) {
+    if (menus[index] === undefined) {
+        return
+    }
+
+    const menu = menus[index]
+    const foundRecursiveMenu = findMenu(menu, name)
+    const foundRecursiveMenus = solveMenus(menus, name, ++index)
+    return foundRecursiveMenu ?? foundRecursiveMenus
+}
+
+/* Resolve os menus e seus filhos */
+function findMenu(menu, name, index = 0) {
+    if (menu.name === name) {
+        return menu.name
+    }
+
+    let found = undefined
+    for (item of menu.items) {
+        const menuRecursiveFound = findMenu(item, name, ++index)
+        found = found ?? menuRecursiveFound
+    }
+    return found
+}
